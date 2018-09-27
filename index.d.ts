3af77337c0af261f74a873a66e2716721eed9db7
bluehexagons <bluehexagons@users.noreply.github.com>
2018-09-27 15:22:34 -0500

    Fix to index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -3,10 +3,10 @@ import OggEncoder from './lib/encoder'
 import OggPacket from './lib/packet'
 
 declare namespace Ogg {
-  export const ogg_packet = OggPacket
-  export const packet = OggPacket
-  export const Decoder = OggDecoder
-  export const Encoder = OggEncoder
+  export const ogg_packet: OggPacket
+  export const packet: OggPacket
+  export const Decoder: OggDecoder
+  export const Encoder: OggEncoder
 }
 
 export default Ogg
